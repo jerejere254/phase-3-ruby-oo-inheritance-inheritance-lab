06c5f1439cbb1bc5f41c347a6fa7a2a9d7dd3f34
jerryjohns <jerry.johns@student.moringaschool.com>
2022-10-24 18:54:19 +0300

    add

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative './user'
+class Student < User
+    attr_reader :knowledge
+    
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(added_string)
+        @knowledge << added_string
+    end
 
 end
